fecbe506cf226f8910da00c4f4ae5cdc87759c79
oli <oli.meier@bluewin.ch>
2024-04-02 11:57:52 +0200

    Added pointLight intensity
    pointLight was too weak on some newer versions of three.js (0.163.0). See: https://stackoverflow.com/questions/77625732/threejs-pointlight-not-lighting-up-my-geometries

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ scene.add(torus);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffffff, 100);
 pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
